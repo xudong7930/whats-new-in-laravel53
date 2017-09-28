c8f42bfbd2fdb4c2a2594c182d1e09c1a924402e
xudong7930 <xudong7930@gmail.com>
2017-09-28 13:59:12 +0800

    laraval pagination

---
+++ resources/views/users.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://ehd4.f3322.net/youtube/bootstrap337/css/bootstrap.min.css">
+</head>
+<body>
+    @foreach ($users as $user)
+        <li>{{ $user->name }}</li>
+    @endforeach
+
+    {{ $users->links() }}
+</body>
+</html>
